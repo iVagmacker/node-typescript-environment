d852cb7a5c570802d262aac72926e90ee7644990
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-07-01 19:56:18 -0300

    Debugando NodeJs

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,8 @@ import routes from './routes';
 const app = express();
 
 app.use(routes);
+app.use(express.json());
 
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack' }),
-);
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
